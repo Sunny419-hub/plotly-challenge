d0fd5b73e8546bedc56c4e689b2ef551fb7f4bf2
SoniaSu <frutos.rojos17@gmail.com>
2021-06-27 22:07:45 -0500

    update

--- static/js/app.js
+++ static/js/app.js
@@ -20,14 +20,37 @@ function optionChanged(value){
             }
         });
         var otus_ids = sample.otu_ids;
-        var sorted = otus_ids.sort((a, b) => b - a);
+        var sorted = otus_ids.sort((a, b) => a - b);
         var sliced = sorted.slice(0, 10);
         console.log(sliced);
+
+        // Trace1 for the Greek Data
+        var trace1 = {
+            x: sliced.map(object => object),
+            y: sliced.map(object => 'OTN '+object),
+            text: sliced.map(object => object),
+            name: "OTU",
+            type: "bar",
+            orientation: "h"
+        };
+        
+        // data
+        var data2 = [trace1];
+        
+        // Apply the group bar mode to the layout
+        var layout = {
+            title: "Top 10 operational taxonomic units (OTUs)",
+            margin: {
+            l: 100,
+            r: 100,
+            t: 100,
+            b: 100
+            }
+        };
+        
+        // Render the plot to the div tag with id "plot"
+        Plotly.newPlot("bar", data2, layout);
     });
     
 }
 
-function filterSample(values){
-    
-    return values.samples.id = select.node().value;
-}
